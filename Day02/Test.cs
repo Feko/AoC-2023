4c9bf7b937a214d878ab711670fdd5f0c385ec1d
Fernando Medeiros <fekomedeiros@gmail.com>
2023-12-02 07:32:07 +0000

    day 02, part 02

--- Day02/Test.cs
+++ Day02/Test.cs
@@ -1,4 +1,6 @@
 
+using Newtonsoft.Json.Linq;
+
 namespace AOC2023.Day02;
 
 public class Test
@@ -6,8 +8,8 @@ public class Test
     [Fact]
     public void Part1()
     {
-        // var lines = File.ReadAllLines("/home/feko/src/dotnet/aoc2023/AoC-2023/Day02/sample.txt");
-        var lines = File.ReadAllLines("/home/feko/src/dotnet/aoc2023/AoC-2023/Day02/input.txt");
+        var lines = File.ReadAllLines("/home/feko/src/dotnet/aoc2023/AoC-2023/Day02/sample.txt");
+        // var lines = File.ReadAllLines("/home/feko/src/dotnet/aoc2023/AoC-2023/Day02/input.txt");
         Dictionary<string, int> constraints = new()
         {
             {"red", 12},
@@ -19,6 +21,17 @@ public class Test
         Assert.Equal(8, result);
     }
 
+    [Fact]
+    public void Part2()
+    {
+        var lines = File.ReadAllLines("/home/feko/src/dotnet/aoc2023/AoC-2023/Day02/sample.txt");
+        //var lines = File.ReadAllLines("/home/feko/src/dotnet/aoc2023/AoC-2023/Day02/input.txt");
+       
+        int result = lines.Select(x => FindGamePower(x)).Sum();
+        Assert.Equal(2286, result);
+    }
+
+   
     private int IdGamePossible(string line, Dictionary<string, int> constraints)
     {
         var parts = line.Split(':');
@@ -34,4 +47,24 @@ public class Test
         }
         return gameId;
     }
+
+    private int FindGamePower(string line)
+    {
+        Dictionary<string, int> requiredAmounts = new()
+        {
+            {"red", 0},
+            {"green", 0},
+            {"blue", 0},
+        };
+        foreach(var game in line.Split(':').Last().Split(';'))
+        {
+            foreach(var cubeset in game.Split(','))
+            {
+                var valuepair = cubeset.Trim().Split(' ');
+                requiredAmounts[valuepair.Last()] = Math.Max(requiredAmounts[valuepair.Last()], Convert.ToInt32(valuepair.First()));
+            }
+        }
+        return requiredAmounts.Values.Aggregate(1, (x,y) => x * y);
+    }
+
 }
