35352f6d5370f31965d6d313aba6e971b3cdee09
Fernando Medeiros <fekomedeiros@gmail.com>
2023-12-21 15:01:05 +0000

    Day21, wip for Part2

---
+++ Day21/TestPart2.cs
@@ -0,0 +1,112 @@
+
+namespace AOC2023.Day21;
+
+public partial class TestPart2
+{
+    public record TileCoords(int RowStart, int RowFinish, int ColumnStart, int ColumnFinish);
+    static (int Height, int Width) MazeSize = default;
+    static int ExpandFactor = 5;
+    static int FinalSteps = 26501365;
+
+
+    [Fact]
+    public void Part2()
+    {
+        var lines = File.ReadAllLines("C:\\DEV\\AoC-2023\\Day21\\input.txt").Select(s => s.ToCharArray()).ToArray(); 
+        MazeSize = (lines.Length, lines[0].Length);
+
+        // Some tests
+        var tileA = GetTileAt(0, 0);
+        var tileB = GetTileAt(0, 5);
+        var tileC = GetTileAt(5, 5);
+        var tileD = GetTileAt(6, 6);
+
+        int result = PerformHugeAssAmountOfSteps(lines);
+        Assert.Equal(1, result);
+    }
+
+    private int PerformHugeAssAmountOfSteps(char[][] array)
+    {
+        HashSet<(int row, int column)> positions = new();
+        (int row,int col) start = GetStartPosition(array);
+        positions.Add(start);
+        int steps = MazeSize.Height * ExpandFactor;
+
+        // Right, this will run for a few seconds and map all the possible positions after repeating for (MAZE_SIZE * 5) steps.
+        // This should consider a grid with 11 x 11 tiles
+        for(int i=0; i<steps; i++ )
+        {
+            HashSet<(int row, int column)> newPositions = new();
+            foreach(var position in positions)
+            {
+                foreach(var neighbour in GetNeighbours(position, array))
+                    newPositions.Add(neighbour);
+            }
+
+            positions = newPositions;
+        }
+
+        // So we have two types of "inner" tiles, even and odd. Let's count the amount of possible positions for each one.
+        int countOdd = CountPositionsForTile(GetTileAt(ExpandFactor, ExpandFactor), positions);
+        int countEven = CountPositionsForTile(GetTileAt(ExpandFactor, ExpandFactor + 1), positions);
+
+        // We also have 4 corners, I'm counting them individually
+        int cornerTop = CountPositionsForTile(GetTileAt(0, ExpandFactor), positions);
+        int cornerLeft = CountPositionsForTile(GetTileAt(ExpandFactor, 0), positions);
+        int cornerBottom = CountPositionsForTile(GetTileAt(ExpandFactor*2, ExpandFactor), positions);
+        int cornerRight = CountPositionsForTile(GetTileAt(ExpandFactor, ExpandFactor*2), positions);
+
+        // Now, we also have some Diagonals which we should consider
+        int diagonalTopLeft = CountPositionsForTile(GetTileAt(ExpandFactor -1, 1), positions);
+        int diagonalBottomLeft = CountPositionsForTile(GetTileAt(ExpandFactor +1, 1), positions);
+        int diagonalTopRight = CountPositionsForTile(GetTileAt(ExpandFactor - 1, ExpandFactor * 2 -1), positions);
+        int diagonalBottomRight = CountPositionsForTile(GetTileAt(ExpandFactor + 1, ExpandFactor * 2 -1), positions);
+
+        // Just to ensure the diagonals are equal
+        int diagonalTopLeft2 = CountPositionsForTile(GetTileAt(ExpandFactor - 2, 2), positions);
+        bool equal = diagonalTopLeft == diagonalTopLeft2;
+
+
+        return positions.Count;
+    }
+
+    private int CountPositionsForTile(TileCoords tileCoords, HashSet<(int row, int column)> positions)
+    {
+        return positions.Count(p => tileCoords.RowStart <= p.row && tileCoords.RowFinish >= p.row 
+            && tileCoords.ColumnStart <= p.column && tileCoords.ColumnFinish >= p.column );
+    }
+
+    private (int, int) GetStartPosition(char[][] lines)
+    {
+        for(int i=0; i < lines.Length; i++)
+            if(Array.IndexOf(lines[i], 'S') >= 0)
+                return (i, Array.IndexOf(lines[i], 'S'));
+        throw new Exception("Start position not found");
+    }
+   
+    private List<(int,int)> GetNeighbours((int Row, int Column) position, char[][] array)
+    {
+        var neighbours = new List<(int row, int column)> { (position.Row +1, position.Column),
+            (position.Row -1, position.Column), (position.Row, position.Column + 1), (position.Row, position.Column -1) };
+        return neighbours.Where(x => IsWalkable(array, x)).ToList();
+    }
+
+    private bool IsWalkable(char[][] array, (int row, int column) x)
+    {
+        int row = CycleRow(x.row);
+        int col = CycleColumn(x.column);
+        return array[row][col] != '#';
+    }
+
+    public TileCoords GetTileAt(int tileRow, int tileColumn)
+    {
+        int start = MazeSize.Width * -1 * ExpandFactor;
+        int startRow = start + (tileRow * MazeSize.Height);
+        int startColumn = start + (tileColumn * MazeSize.Width);
+        return new TileCoords(startRow, startRow + MazeSize.Height - 1, startColumn, startColumn + MazeSize.Width - 1);
+    }
+
+    public int CycleRow(int row) => row % MazeSize.Height >= 0 ? row % MazeSize.Height : MazeSize.Height + (row % MazeSize.Height);
+    public int CycleColumn(int column) => column % MazeSize.Width >= 0 ? column % MazeSize.Width : MazeSize.Width + (column % MazeSize.Width);
+}
+
